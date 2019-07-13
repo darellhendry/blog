4b3ea8470fffd06a872bd432fa1415ccee692bf7
Darell Hendry <1darellhendry@gmail.com>
2019-07-13 10:37:33 +0700

    Updates

---
+++ precache-manifest.2220823805d891de12c0df94f9ad82e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de656cb4fd49246ce2b6ec3af4acaa36",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "413c060dff457a28f344",
+    "url": "/blog/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "e2694439db0362cda476",
+    "url": "/blog/static/css/main.6dc21f51.chunk.css"
+  },
+  {
+    "revision": "413c060dff457a28f344",
+    "url": "/blog/static/js/2.a8749a65.chunk.js"
+  },
+  {
+    "revision": "e2694439db0362cda476",
+    "url": "/blog/static/js/main.8647d420.chunk.js"
+  },
+  {
+    "revision": "eeecc51c3b4e3b0a67a2",
+    "url": "/blog/static/js/runtime~main.516a48d0.js"
+  },
+  {
+    "revision": "7233d5e51719209b0b8910ac76ca1d8f",
+    "url": "/blog/static/media/00.7233d5e5.jpg"
+  }
+]);
