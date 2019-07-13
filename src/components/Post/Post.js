ed1c0a27908a7bb9e9aea3306172294f14b52860
Darell Hendry <1darellhendry@gmail.com>
2019-07-13 10:31:49 +0700

    breadcrumb

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { posts } from '../../text/text'
 import {StyleSheet, css } from 'aphrodite'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Breadcrumb } from 'react-bootstrap'
 import image from '../../assets/images/00.jpg'
 
 export default class Post extends React.Component {
@@ -10,6 +10,10 @@ export default class Post extends React.Component {
     this.setState(prev => {
       return { isIn: !prev.isIn };
     })
+  }
+
+  goToHome = () => {
+    this.props.history.goBack()
   }
   render() {
     let post = {title: '', content: ''}
@@ -22,12 +26,26 @@ export default class Post extends React.Component {
     
     return (
       <>
-        <img className={css(styles.image)} src={image} alt='Proklamasi'/>
+        <Breadcrumb
+          className={css(styles.breadcrumb)}
+          bsPrefix="breadcrumb custom-bs"
+        >
+          <Breadcrumb.Item onClick={this.goToHome}>
+            Home
+          </Breadcrumb.Item>
+          <Breadcrumb.Item active>{post.title}</Breadcrumb.Item>
+        </Breadcrumb>
+        <img className={css(styles.image)} src={image} alt="Proklamasi" />
         <Container>
           <Row>
             <Col>
-              <h5 style={{fontWeight: 'bold'}} className='h5'>{post.title}</h5>
-              <p>{post.content}</p>
+              <div className={css(styles.titleContainer)}>
+                <h5 style={{ fontWeight: "bold" }} className="h5">
+                  {post.title}
+                </h5>
+                <em>{post.time}</em>
+              </div>
+              <div className="p">{post.content}</div>
             </Col>
           </Row>
         </Container>
@@ -37,9 +55,17 @@ export default class Post extends React.Component {
 }
 const styles = StyleSheet.create({
   image: {
-    width: '100%',
+    width: "100%",
     height: 450,
-    objectFit: 'cover',
-    marginBottom: 34
+    objectFit: "cover",
+    marginBottom: 34,
+    borderRadius: 8
   },
-})
+  titleContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    fontSize: 18,
+    paddingBottom: 24
+  }
+});
--- src/text/text.js
+++ src/text/text.js
@@ -1,3 +1,5 @@
+import React from 'react'
+
 export const thumbnails = [
   {
     id: 0,
@@ -10,7 +12,18 @@ export const posts = [
          {
            id: 0,
            title: "Proklamasi",
-           content:
-             "Pukul 00: 42 web ini saya proklamasikan telah memposting postingan pertama saya."
+           time: 'Jul 12, 2019',
+           content: (
+             <>
+              <p>
+                Pukul 00: 42 blog ini saya proklamasikan telah memposting
+                postingan pertama saya.
+              </p>
+              <p>
+                Sebagai permulaan saya ingin menyampaikan blog ini mungkin bisa tidak bermanfaat, tetapi jika bermanfaat, saya tidak akan peduli :) .
+                Tujuan dibuatnya blog ini untuk mengisi <em>leisure time</em> dan <em>sharing</em> hobi saya.
+              </p>
+             </>
+           )
          }
        ];
